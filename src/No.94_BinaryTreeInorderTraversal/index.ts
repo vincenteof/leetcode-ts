402d14031a7fb6add2ea0e06862b8b011293a963
vincenteof <vincenteof@qq.com>
2021-04-03 11:10:53 +0800

    feat: 94

--- src/No.94_BinaryTreeInorderTraversal/index.ts
+++ src/No.94_BinaryTreeInorderTraversal/index.ts
@@ -5,23 +5,41 @@ import { TreeNode } from '../types'
  */
 function inorderTraversal(root: TreeNode | null): number[] {
   const result = []
-  function recur(cRoot: TreeNode | null) {
-    if (cRoot === null) {
-      return
-    }
-    if (cRoot.left) {
-      recur(cRoot.left)
-    }
-    result.push(cRoot.val)
-    if (cRoot.right) {
-      recur(cRoot.right)
+  const stack = []
+  let p = root
+  while (stack.length > 0 || p !== null) {
+    if (p) {
+      stack.push(p)
+      p = p.left
+    } else {
+      p = stack.pop()
+      result.push(p.val)
+      p = p.right
     }
   }
-  recur(root)
   return result
 }
+
+// function inorderTraversal(root: TreeNode | null): number[] {
+//   const result = []
+//   function recur(cRoot: TreeNode | null) {
+//     if (cRoot === null) {
+//       return
+//     }
+//     if (cRoot.left) {
+//       recur(cRoot.left)
+//     }
+//     result.push(cRoot.val)
+//     if (cRoot.right) {
+//       recur(cRoot.right)
+//     }
+//   }
+//   recur(root)
+//   return result
+// }
 
 /**
  * solution:
  * We use a global variable to store.
+ * We do a small work once an iteration in a non-recursive solution.
  */
