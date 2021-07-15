eae4e89c41f9e34a4a4f14898ea4a1778004bd6c
vincenteof <vincenteof@qq.com>
2021-07-15 23:04:56 +0800

    feat: 96

--- src/No.96_UniqueBinarySearchTrees/index.ts
+++ src/No.96_UniqueBinarySearchTrees/index.ts
@@ -3,5 +3,21 @@
  * @param n
  */
 function numTrees(n: number): number {
-  return 1
+  const results = [1]
+  for (let i = 1; i <= n; i++) {
+    const remaining = i - 1
+    let sum = 0
+    for (let j = 0; j <= remaining; j++) {
+      const l = j
+      const r = remaining - l
+      sum += results[l] * results[r]
+    }
+    results[i] = sum
+  }
+  return results[n]
 }
+
+/**
+ * solution:
+ * For n, we find all pairs [x, n - 1 - x], numTrees[n] = sum(numTrees[x] * numTrees[n - 1 - x])
+ */
